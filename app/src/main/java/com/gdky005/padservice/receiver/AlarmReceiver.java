9a1520ae4ecd3e281d03140e8d2eccb9acc15dd4
wangqing <wangqing@kaolafm.com>
2016-03-02 10:14:37 +0800

    1.添加闹钟功能；

---
+++ app/src/main/java/com/gdky005/padservice/receiver/AlarmReceiver.java
@@ -0,0 +1,48 @@
+package com.gdky005.padservice.receiver;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
+import com.gdky005.padservice.MainActivity;
+import com.gdky005.padservice.R;
+import com.gdky005.padservice.utils.L;
+
+/**
+ * Created by WangQing on 16/2/29.
+ */
+public class AlarmReceiver extends BroadcastReceiver {
+
+    private NotificationManager manager;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        L.i("收到闹钟提醒了");
+
+        long time = System.currentTimeMillis();
+
+        manager = (NotificationManager) context.getSystemService(android.content.Context.NOTIFICATION_SERVICE);
+        //例如这个id就是你传过来的
+        String id = intent.getStringExtra("id");
+        //MainActivity是你点击通知时想要跳转的Activity
+        Intent playIntent = new Intent(context, MainActivity.class);
+        playIntent.putExtra("id", id);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, String.valueOf(time).hashCode(), playIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        builder.setContentTitle("title：" + context.getString(R.string.app_name))
+                .setContentText("提醒时间:" + time)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true)
+                .setSubText("包名：" + context
+                .getPackageName());
+        manager.notify(1, builder.build());
+    }
+}
